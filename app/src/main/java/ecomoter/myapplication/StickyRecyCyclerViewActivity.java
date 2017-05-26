9eb490deabe1e497b47c488d04a61a2910e18c62
biao.huang <123456>
2017-05-26 09:49:33 +0800

    2017年5月26日09:49:30

--- app/src/main/java/ecomoter/myapplication/StickyRecyCyclerViewActivity.java
+++ app/src/main/java/ecomoter/myapplication/StickyRecyCyclerViewActivity.java
@@ -62,7 +62,6 @@ public class StickyRecyCyclerViewActivity extends AppCompatActivity {
                 View stickyInfoView = recyclerView.findChildViewUnder(mHead.getMeasuredWidth() / 2, 5);
                 mRv.getChildAt(0).getId();
                 Log.d("type1type1111",stickyInfoView.getId()+"");
-
                 Log.d("type1type111",mRv.getChildAt(0).getId()+"");
                 Log.d("测试测试","ddddd");
 //                Toast.makeText(StickyRecyCyclerViewActivity.this, mAdapter.getItemViewType(manager.findFirstCompletelyVisibleItemPosition()),Toast.LENGTH_SHORT).show();
